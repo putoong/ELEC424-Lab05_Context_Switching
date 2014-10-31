eddb046cc9b6ba94253d3fa2f7bd8c4204379356
Pu Dong <kraintherain@gmail.com>
2014-10-31 03:19:37 -0500

    Init complete

---
+++ inc/systick_context_switcher.h
@@ -0,0 +1,37 @@
+#ifndef SYSTICK_CONTEXT_SWITCHER_H_
+#define SYSTICK_CONTEXT_SWITCHER_H_
+
+#define STACK_SIZE 1024
+
+typedef struct {
+	void * sp;
+} TCB ;
+
+typedef struct  {
+	uint32_t r0;
+	uint32_t r1;
+	uint32_t r2;
+	uint32_t r3;
+	uint32_t r12;
+	uint32_t lr;
+	uint32_t pc;
+	uint32_t psr;
+} hstack;
+
+typedef struct  {
+	uint32_t r4;
+	uint32_t r5;
+	uint32_t r6;
+	uint32_t r7;
+	uint32_t r8;
+	uint32_t r9;
+	uint32_t r10;
+	uint32_t r11;
+} sstack ;
+
+void SysTick_Handler(void);
+void create_task(void (*p) (void ), void *, int);
+void run_task(void);
+void switch_thread_context (void);
+
+#endif
---
+++ src/systick_context_switcher.c
@@ -0,0 +1,98 @@
+#include "systick_context_switcher.h"
+#include "sys_init.h"
+
+TCB TCB_table[2];
+
+static inline void * read_MSP (void){
+	void * p;
+	asm volatile (
+		"MRS %0, msp\n\t" 
+		: "=r" (p)
+		);
+}
+
+static inline void save_thread_context (void){
+	uint32_t p;
+	asm volatile (
+		"MRS %0, psp\n\t"
+		"STMDB %0!, (r4-r11)\n\t"
+		"MSR %0, psp\n\t"
+		: "=r" (p)
+		);
+}
+
+static inline void load_thread_context (void){
+	uint32_t p;
+	asm volatile (
+		"MRS %0, psp\n\t"
+		"LDMFD %0!, (r4-r11)\n\t"
+		"MSR psp, %0\n\t"
+		: "=r" (p)
+		);
+}
+
+void SysTick_Handler(void){ // set the system tick handler?
+	if (counter == 49) {
+		//LEDToggle();
+		save_thread_context();
+		switch_thread_context();
+		load_thread_context();
+	}
+	counter = (counter + 1) % 50;
+}
+
+static inline void * read_PSP (){
+	void * p;
+	asm volatile (
+		"MRS %0, psp\n\t"
+		: "=r" (p)
+		);
+	return p;
+}
+
+static inline void write_PSP (void * p){
+	asm volatile (
+		"MSR psp, %0\n\t" : : "r" (p)
+		);
+}
+
+static char mem[STACK_SIZE];
+
+void switch_thread_context (void){
+	TCB_table[current].sp = read_PSP ();
+	current = 1 - current;
+	void * psp = (TCB_table[current]).sp;
+	write_PSP (psp);
+}
+
+void init_tasks(void) {
+	create_task( task_blink_led, mem, 0);
+	create_task( task_spin_motors, mem + STACK_SIZE, 1);
+}
+
+void create_task(void (*p)(void) , void * s_addr, int idx) {
+	
+	hstack * hs;
+	hs = (hstack *) (s_addr + STACK_SIZE - sizeof(hstack));
+	hs -> psr = 0x21000000;
+	hs -> pc = ((uint32_t)p);
+	hs -> lr = 0;
+	hs -> r12 = 0;
+	hs -> r3 = 0; 
+	hs -> r2 = 0;
+	hs -> r1 = 0;
+	hs -> r0 = 0;
+
+	TCB_table[idx].sp = s_addr + STACK_SIZE - sizeof(hstack) - sizeof(sstack);
+
+}
+
+void run_task() {
+	uint32_t sp = (uint32_t) TCB_table[0].sp + sizeof(sstack) + sizeof(hstack);
+	hstack  * hs = (hstack *) (mem + STACK_SIZE - sizeof(hstack));
+	asm volatile("MOVhs -> pc = ((uint32_t)p r12, #2\n\t");
+	asm volatile("MSR control, r12\n\t");
+	asm volatile("MSR psp, %0\n\t" : "=r" (sp) );
+	asm volatile("BX %0\n\t" : : "r" (hs->pc));
+
+}
